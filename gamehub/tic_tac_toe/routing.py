bdfd9cb2ff0a207a5e4562498b3f3a3e82c32796
Kevin Hernandez <kevin.davidhr@gmail.com>
2024-04-23 18:00:38 -0600

    feat: Add WebSocket service for Chinese checkers

---
+++ gamehub/tic_tac_toe/routing.py
@@ -0,0 +1,7 @@
+# tic_tac_toe/routing.py
+from django.urls import re_path
+from .consumers import TicTacToeConsumer
+
+websocket_urlpatterns = [
+    re_path(r'ws/tictactoe/(?P<room_name>\w+)/$', TicTacToeConsumer.as_asgi()),
+]
